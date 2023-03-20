004d4a3b4c6d4f2202ca6660e442ca4ba890761e
yuuuki2 <saschazhang@gmail.com>
2023-03-20 14:59:14 +0100

    Es rechnent die Wertemenge aus

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 funktion = input("Geben Sie eine Funktion (y = a * x + b) ein: ")
 
@@ -8,23 +7,34 @@ y, rest = funktion.split(" = ")
 a, rest = rest.split(" * ")
 x, b = rest.split(" + ")
 
-def y(a,x,b):
-    new_y = a * x + b
-
-    return new_y
-
 def definitionsmenge():
-    d_menge = np.linspace(-10,10,num=20)
+    d_menge = [-10,-9,-8,-7,-6,-5,-4,-3,-2,1,0,1,2,3,4,5,6,7,8,9,10]
 
     return d_menge
 
+def wertemenge(a,b,d_menge):
+    #new_y = a * x + b
+    w_menge = []
+    for x in d_menge:
+       new_y = int(a) * x + int(b)
+       w_menge.append(new_y) 
 
 
+    return w_menge
 
 
 
 
 
 def main():
-    pass
+    d_menge = definitionsmenge()
+    w_menge = wertemenge(a,b,d_menge)
 
+    print(w_menge)
+    
+    plt.plot(d_menge,w_menge)
+    plt.xlabel("Definitionsmenge")
+    plt.ylabel("Wertemenge")
+    plt.show()
+    
+main()

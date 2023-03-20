54e5cd38132370f49c5f4ac4336f5a56a6229746
yuuuki2 <saschazhang@gmail.com>
2023-03-20 13:56:22 +0100

    main.py erstellt und geadded

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+d_wete_liste = []
+w_werte_liste = []
+
+d_werte = int(input("Geben Sie die Definitionswere an: "))
+w_werte = int(input("Geben Sie die Wertemenge an: "))
+
+d_wete_liste.append(d_werte)
+w_werte_liste.append(w_werte)
+
+
+
